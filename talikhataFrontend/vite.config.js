1d051df9d581f54b5619f8b9ed6dbc0ff031c9e7
delowar-Hosen <delowarhosen15o1121@gmail.com>
2025-07-13 16:18:20 +0600

    error-solved

--- talikhataFrontend/vite.config.js
+++ talikhataFrontend/vite.config.js
@@ -3,9 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-  plugins: [react()],
+  plugins: [react(), tailwindcss()], // একটি plugins অ্যারেতে দুটি প্লাগিন একসাথে
+  base: '/', // রুট ডোমেইনের জন্য; সাবডিরেক্টরির জন্য '/subpath/' ব্যবহার করুন
   build: {
-    outDir: "dist",
+    outDir: "dist", // Netlify-র জন্য ডিফল্ট আউটপুট ডিরেক্টরি
   },
 });
